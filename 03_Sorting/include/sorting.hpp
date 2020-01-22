5be50a9ba404db62739a26077cb336d619954f83
laurabalasso <laurabalasso96@gmail.com>
2020-01-22 16:59:19 +0100

    Counting sort added

--- 03_Sorting/include/sorting.hpp
+++ 03_Sorting/include/sorting.hpp
@@ -8,5 +8,7 @@ int Partition(int * A, int i, int j, int p);
 
 void Quick_Sort(int * A, int N, int l = 0);
 
+void Counting_Sort(int * A, int * B,  int sizeA);
+
 
 #endif /* sorting_hpp */
--- 03_Sorting/sorting_main.cpp
+++ 03_Sorting/sorting_main.cpp
@@ -5,29 +5,38 @@
 
 int main(){
     
-    std::cout << ">>>> Insertion sort <<<< \n";
-    std::cout << "Inster the size of the array to sort: \n";
-    int size;
-    std::cin >> size;
+    int size = 10;
     int * A{new int[size]};
-    std::cout << "Insert the array's elements\n";
-    for(int i = 0; i < size; i++){
-        std::cin >> A[i];
-    }
+    
+    /* INSERTION SORT */
+    
+    Random_Fill(A, size, 20);
     
     insertion_sort(A, size);
     
-    std::cout << "Sorted array: \n";
+    std::cout << "Insertion sort: \n";
     
     Array_Print(A, size);
     
-    int size2 = 8;
-    int * Array = new int[size2];
-    for(int i{0}; i < size2; i++){
-        Array[i] = size2 - i;
-    }
-    Array_Print(Array, size2);
-    Quick_Sort(Array, size2);
-    Array_Print(Array, size2);
+    /*  QUICK SORT */
     
+    Random_Fill(A, size, 20);
+    
+    Quick_Sort(A, size);
+    
+    std::cout << "Quick sort: \n";
+    
+    Array_Print(A, size);
+    
+    /* COUNTING SORT */
+    
+    Random_Fill(A, size, 20);
+    
+    int * B = Array_Alloc(size);
+    
+    Counting_Sort(A, B, size);
+    
+    std::cout << "Counting sort: \n";
+    
+    Array_Print(B, size);
 }
