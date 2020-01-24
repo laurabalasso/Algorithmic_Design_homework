8d76f5eaf79c6387bf683f37d64ae91356f38d4e
laurabalasso <laurabalasso96@gmail.com>
2020-01-24 10:48:29 +0100

    added utilities for radix sort

--- 03_Sorting/include/array_utilities.hpp
+++ 03_Sorting/include/array_utilities.hpp
@@ -10,7 +10,10 @@ void swap(int * A, const int& i ,const int& j);
 /*
 function to print an array of a given size
 */
+
 void Array_Print(int* A, int N);
+
+void Array_Print(double * A, int N);
 
 /*
 function to allocate an array dynamically and initialize its elements to zero
@@ -31,6 +34,25 @@ int Max(int * A, int size);
 function to fill an array with random numbers between 1 and rand_max
  */
 void Random_Fill(int * A, int size, int rand_max);
+
+void Random_Fill_Double(double * A, int size);
+
+
+
+struct Node {
+    double value;
+    struct Node * next;
+};
+
+void Swap_Node( Node * a, Node * b);
+
+void Bucket_Print(Node * const head);
+
+int List_Size(Node * const head);
+
+Node * append(Node *head, const double value);
+
+double Get_Value_List(Node * head, int i);
 
 #endif
 
--- 03_Sorting/src/array_utilities.cpp
+++ 03_Sorting/src/array_utilities.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 #include "../include/array_utilities.hpp"
 
 
@@ -8,7 +9,7 @@ void swap(int * A, const int& i ,const int& j){
     A[j] = temp;
 }
 
-void Array_Print(int* A, int N){
+void Array_Print(int * A, int N){
 
     for(int i{0}; i < N; i++){
             std::cout<< A[i] << "   ";
@@ -16,6 +17,16 @@ void Array_Print(int* A, int N){
 
     std::cout<< "\n";
 }
+
+void Array_Print(double * A, int N){
+    
+    for(int i{0}; i < N; i++){
+        std::cout<< A[i] << "   ";
+    }
+    
+    std::cout<< "\n";
+}
+
 
 int * Array_Alloc(int size){
     int * A = new int[size];
@@ -46,10 +57,72 @@ int Max(int * A, int size){
     return max;
 }
 
-void Random_Fill(int * A, int size, int rand_max){
+void Random_Fill(int * A, int size, int rand_max ){
     
     for(int i = 0; i < size; i++){
         A[i] = (rand() % rand_max) + 1;
     }
     
 }
+
+void Random_Fill_Double(double * A, int size){
+    
+    for(int i = 0; i < size; i++){
+        A[i] = ((double) rand()/(RAND_MAX));
+    }
+    
+}
+
+void Swap_Node( Node * a, Node * b){
+    double tmp = a->value;
+    a->value = b->value;
+    b->value = tmp;
+}
+
+
+void Bucket_Print(Node * const head){
+    if(!head) return;
+    Node *aux = head;
+    while (aux->next){
+        std::cout <<  aux->value << std::endl;
+        aux = aux->next;
+    }
+    std::cout <<  aux->value << std::endl;
+    }
+
+int List_Size(Node * const head){
+    int size = 0;
+    if(!head) return size;
+    Node *aux = head;
+    while (aux->next) {
+        aux=aux->next;
+        size++;
+    }
+    size++;
+    return size;
+}
+
+Node * append(Node *head, const double value){
+    Node *newNode = (Node*) malloc(sizeof(Node));
+    newNode->value = value;
+    newNode->next = NULL;
+    
+    if (head){
+        Node *aux = head;
+        while (aux->next) aux = aux->next;
+        aux->next = newNode;
+    }
+    else head = newNode;
+    
+    return head;
+}
+
+
+double Get_Value_List(Node * head, int i){
+    Node *aux = head;
+    int idx = 0;
+    while (idx++ < i )
+        aux = aux->next;
+    
+    return aux-> value;
+    }
