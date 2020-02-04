4a6a0687e9477ce69fa4b13b15a96e4793cb096f
laurabalasso <laurabalasso96@gmail.com>
2020-02-04 09:24:25 +0100

    improved memory allocation

--- 03_Sorting/sorting_tests.cpp
+++ 03_Sorting/sorting_tests.cpp
@@ -1,24 +1,32 @@
-#include <iostream>
-#include <climits>
 #include <time.h>
-#include <iomanip>
+#include <climits>
+#include <iostream>
+
 #include "include/array_utilities.hpp"
 #include "include/sorting.hpp"
 
+using namespace std;
+
+double get_execution_time(const struct timespec b_time,
+                          const struct timespec e_time)
+{
+    return (e_time.tv_sec-b_time.tv_sec) +
+    (e_time.tv_nsec-b_time.tv_nsec)/1E9;
+    
+}
 
 int main(){
     
-    /* Arrays initialization */
-    const int NUM_SIZE = 6;
-    int size[NUM_SIZE] = {10000,20000,40000,60000,80000,90000};
+    const int NUM_SIZE = 5;
+    int sizes[NUM_SIZE] = {10000, 20000, 40000, 60000, 80000};
     
     int * insertionArray;
-    int * bestInsertion;
-    int * worstInsertion;
+    int * bestInsertionArray;
+    int * worstInsertionArray;
     
-    int * quickArray ;
-    int * bestQuick;
-    int * worstQuick;
+    int * quickArray;
+    int * bestQuickArray;
+    int * worstQuickArray;
     
     int * countArray;
     int * radixArray;
@@ -28,154 +36,129 @@ int main(){
     
     /* Time measures */
     
-    double insertionTime, insertionTimeBest, insertionTimeWorst;
-    double quickTime, quickTimeBest, quickTimeWorst;
+    double insertionTime, bestInsertionTime, worstInsertionTime;
+    double quickTime, bestQuickTime, worstQuickTime;
     double countingTime, radixTime, bucketTime, heapTime;
+
     
     std::cout << "N \t Insertion Sort \t Worst Insertion \t Best Insertion \t Quick Sort \t Worst Quick \t Best Quick \tCounting Sort\tRadix Sort\t Bucket Sort\t Heap Sort\n" ;
-    
+
     
     for(int i = 0; i < NUM_SIZE; i++){
         
-        int n = size[i];
-        
-        insertionArray = new int[n];
-        bestInsertion = new int[n];
-        worstInsertion = new int[n];
-        
-        quickArray = new int[n] ;
-        bestQuick= new int[n];
-        worstQuick = new int[n];
-        
-        countArray = new int[n];
-        radixArray = new int[n];
-        bucketArray = new double[n];
-        heapArray = new int[n];
-        
-        Random_Fill(insertionArray, n, 100);
-        Random_Fill(quickArray, n, 100);
-        Random_Fill(countArray, n, 100);
-        Random_Fill(radixArray, n, 100);
-        Random_Fill_Double(bucketArray, n);
-        Random_Fill(heapArray, n, 100);
-        
+        int n = sizes[i];
         
         struct timespec start, end;
         
-        /* INSERTION SORT */
+        /* INSERTON SORT */
         
+        insertionArray  = new int[n];
+        Random_Fill(insertionArray, n, 50);
         clock_gettime(CLOCK_MONOTONIC, &start);
         insertion_sort(insertionArray, n);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        insertionTime = (end.tv_sec - start.tv_sec) * 1e9;
-        insertionTime = (insertionTime + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        
+        insertionTime = get_execution_time(start, end);
+        delete[] insertionArray;
         
         /* Worst case: array sorted in descending order */
         
-        fill_descending(worstInsertion, n);
+        worstInsertionArray = new int[n];
+        Random_Fill(worstInsertionArray, n, 100);
+        reverse_order(worstInsertionArray, n);
         clock_gettime(CLOCK_MONOTONIC, &start);
-        insertion_sort(worstInsertion, n);
+        insertion_sort(worstInsertionArray, n);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        insertionTimeWorst = (end.tv_sec - start.tv_sec) * 1e9;
-        insertionTimeWorst = (insertionTimeWorst + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        
+        worstInsertionTime = get_execution_time(start, end);
+        delete[] worstInsertionArray;
         
         /* Best case: array sorted in ascending order */
-        fill_ascending(bestInsertion, n);
+        bestInsertionArray = new int[n];
+        Random_Fill(bestInsertionArray, n, 100);
+        insertion_sort(bestInsertionArray, n);
         clock_gettime(CLOCK_MONOTONIC, &start);
-        insertion_sort(bestInsertion, n);
+        insertion_sort(bestInsertionArray, n);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        insertionTimeBest = (end.tv_sec - start.tv_sec) * 1e9;
-        insertionTimeBest = (insertionTimeBest + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-       
+        bestInsertionTime = get_execution_time(start, end);
+        delete[] bestInsertionArray;
+        
+        
         
         /* QUICK SORT */
         
         /* Average case: random filled array, pivot = first element */
+        quickArray = new int[n];
+        Random_Fill(quickArray, n, 50);
         clock_gettime(CLOCK_MONOTONIC, &start);
         Quick_Sort(quickArray, 0, n-1);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        quickTime = (end.tv_sec - start.tv_sec) * 1e9;
-        quickTime = (quickTime + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+        quickTime = get_execution_time(start, end);
+        delete[] quickArray;
         
-      
+        
         /* Worst case: array is sorted (in ascending or descending order) and the pivot is the fist or the last element */
-        fill_descending(worstQuick, n); //descending order
+        worstQuickArray = new int[n];
+        Random_Fill(worstQuickArray, n, 100);
+        reverse_order(worstQuickArray, n);//descending order
         clock_gettime(CLOCK_MONOTONIC, &start);
-        Quick_Sort(worstQuick, 0, n-1); //first element pivot
+        Quick_Sort(worstQuickArray, 0, n-1); //first element pivot
         clock_gettime(CLOCK_MONOTONIC, &end);
-        quickTimeWorst = (end.tv_sec - start.tv_sec) * 1e9;
-        quickTimeWorst = (quickTimeWorst + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-       
-        
-        
+        worstQuickTime = get_execution_time(start, end);
+        delete[] worstQuickArray;
         
         /* Best case: the pivot is the central element and the array is sorted */
-        fill_ascending(bestQuick, n);
+        bestQuickArray = new int[n];
+        Random_Fill(bestQuickArray, n, 100);
+        insertion_sort(bestQuickArray, n);
         clock_gettime(CLOCK_MONOTONIC, &start);
-        Quick_Sort_Best(bestQuick,0, n-1);
+        Quick_Sort_Best(bestQuickArray,0, n-1);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        quickTimeBest =(end.tv_sec - start.tv_sec) * 1e9;
-        quickTimeBest = (quickTimeBest + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        
-
-        
+        bestQuickTime = get_execution_time(start, end);
+        delete[] bestQuickArray;
         
         /* COUNTING SORT */
+        countArray = new int[n];
+        Random_Fill(countArray, n, 50);
         int * auxArray = Array_Alloc(n);
         clock_gettime(CLOCK_MONOTONIC, &start);
         Counting_Sort(countArray, auxArray, n);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        countingTime =(end.tv_sec - start.tv_sec) * 1e9;
-        countingTime = (countingTime + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        
+        countingTime = get_execution_time(start, end);
+        delete[] countArray;
         
         /* RADIX SORT */
-        
+        radixArray = new int[n];
+        Random_Fill(radixArray, n, 50);
         clock_gettime(CLOCK_MONOTONIC, &start);
         Radix_Sort(radixArray, n);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        radixTime = (end.tv_sec - start.tv_sec) * 1e9;
-        radixTime = (radixTime + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-        
-        
+        radixTime = get_execution_time(start , end);
+        delete[] radixArray;
         
         /* BUCKET SORT */
         
+        bucketArray = new double[n];
+        Random_Fill_Double(bucketArray, n);
         clock_gettime( CLOCK_MONOTONIC, &start);
         Bucket_Sort(bucketArray, n);
         clock_gettime( CLOCK_MONOTONIC, &end);
-        bucketTime = (end.tv_sec - start.tv_sec) * 1e9;
-        bucketTime = (bucketTime + (end.tv_nsec - start.tv_nsec)) * 1e-9;
-       
+        bucketTime = get_execution_time(start, end);
+        delete[] bucketArray;
         
         /* HEAP SORT */
+        heapArray = new int[n];
+        Random_Fill(heapArray, n, 50);
         clock_gettime(CLOCK_MONOTONIC, &start);
         heap_sort(heapArray, n);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        heapTime = (end.tv_sec - start.tv_sec) * 1e9;
-        heapTime = (heapTime + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+        heapTime = get_execution_time(start, end);
         
+        delete[] heapArray;
         
-        /* execution times */
         
         std::cout<< n << "\t  ";
-        std::cout<< std::setprecision(8)  << insertionTime << "\t\t " << insertionTimeWorst << "\t\t  " << insertionTimeBest << "\t\t " << quickTime << "\t   " << quickTimeWorst << "\t " << quickTimeBest << "\t" << countingTime << "\t " << radixTime << "\t  " << bucketTime << "\t " << heapTime  << "\n";
+        std::cout << insertionTime << "\t\t " << worstInsertionTime << "  \t\t  " << bestInsertionTime << "  \t\t " << quickTime << "\t     " << worstQuickTime << "\t " << bestQuickTime << "\t" << countingTime << "  \t " << radixTime << "\t  " << bucketTime << "\t " << heapTime  << "\n";
+
         
-        delete[] insertionArray;
-        delete[] bestInsertion;
-        delete[] worstInsertion;
-        delete[] quickArray;
-        delete[] bestQuick;
-        delete[] worstQuick;
-        delete[] radixArray;
-        delete[] countArray;
-        delete[] bucketArray;
-        delete[] heapArray;
     }
     
-    
-
 }
-
